fbc4d728a9ea0b4fc1d0180f649768adeceeff9a
real gamer <gamer@gamer.gamer.gamer>
2026-01-04 21:38:02 +0800

    sigh

--- tools/ci/download_byond.sh
+++ tools/ci/download_byond.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 source dependencies.sh
-echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR from http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip"
-curl "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
+echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR."
+curl "https://puu.sh/KGrYe/6a71eef0dd.zip" -o C:/byond.zip
