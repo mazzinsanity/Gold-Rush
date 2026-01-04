095275369a65094d0634512c7599a2223f9d1bb6
real gamer <gamer@gamer.gamer.gamer>
2026-01-04 21:06:54 +0800

    WIP on runtimes

--- dependencies.sh
+++ dependencies.sh
@@ -5,19 +5,19 @@
 
 # byond version
 export BYOND_MAJOR=516
-export BYOND_MINOR=1648
+export BYOND_MINOR=1659
 
 #rust_g git tag
-export RUST_G_VERSION=3.5.1
+export RUST_G_VERSION=4.2.0
 
 #node version
 export NODE_VERSION_LTS=22.11.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.8
+export SPACEMAN_DMM_VERSION=suite-1.11
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.9.0
+export PYTHON_VERSION=3.11.0
 
 #auxlua repo
 export AUXLUA_REPO=tgstation/auxlua
